151c531b51203f463fcb9a98332ddbe745e23207
chihabSD <adamchihab2012@hotmail.com>
2019-08-03 05:34:58 +0100

    setting up react project

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { Button } from "reactstrap";
+function App() {
+  return (
+    <div className="App">
+      <h1> Hello world </h1>
+      <Button color="danger">Danger!</Button>
+    </div>
+  );
+}
+
+export default App;
